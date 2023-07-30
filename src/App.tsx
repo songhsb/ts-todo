fff17ae0c53414e204a74d6d705c994a3e3897bf
songhsb <hssong404@gmail.com>
2023-07-30 10:01:19 +0900

    기능 완성

--- src/App.tsx
+++ src/App.tsx
@@ -33,6 +33,10 @@ const App = () => {
 
   const onSubmitHandler = (event: React.FormEvent<HTMLFormElement>): void => {
     event.preventDefault();
+    if (!(title && body)) {
+      return;
+    }
+
     const newTodo: Todo = {
       id: nanoid(),
       title,
@@ -41,6 +45,21 @@ const App = () => {
     };
     setTodos([...todos, newTodo]);
     resetInput();
+  };
+
+  const onClickDelete = (id: string): void => {
+    const newTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(newTodos);
+  };
+
+  const onClickSwitch = (id: string): void => {
+    const newTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.isDone = !todo.isDone;
+      }
+      return todo;
+    });
+    setTodos(newTodos);
   };
 
   return (
@@ -52,16 +71,46 @@ const App = () => {
         <input type="text" value={body} onChange={onChangeBody}></input>
         <button type="submit">추가</button>
       </form>
-      <div>
-        {todos.map((todo) => {
-          return (
-            <div key={todo.id}>
-              <p>{todo.title}</p>
-              <p>{todo.body}</p>
-            </div>
-          );
-        })}
-      </div>
+      <section>
+        <p>isWorking</p>
+        {todos
+          .filter((todo) => {
+            return todo.isDone === false;
+          })
+          .map((todo) => {
+            return (
+              <div key={todo.id}>
+                <p>{todo.title}</p>
+                <p>{todo.body}</p>
+                <button type="button" onClick={() => onClickDelete(todo.id)}>
+                  삭제
+                </button>
+                <button type="button" onClick={() => onClickSwitch(todo.id)}>
+                  완료
+                </button>
+              </div>
+            );
+          })}
+        <p>isDone</p>
+        {todos
+          .filter((todo) => {
+            return todo.isDone === true;
+          })
+          .map((todo) => {
+            return (
+              <div key={todo.id}>
+                <p>{todo.title}</p>
+                <p>{todo.body}</p>
+                <button type="button" onClick={() => onClickDelete(todo.id)}>
+                  삭제
+                </button>
+                <button type="button" onClick={() => onClickSwitch(todo.id)}>
+                  취소
+                </button>
+              </div>
+            );
+          })}
+      </section>
     </>
   );
 };
